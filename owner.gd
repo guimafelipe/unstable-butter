d8a7b9bddf15e5fc6b43de151e283227d0f13262
Felipe <guima.felipec@gmail.com>
2025-12-31 14:48:32 -0800

    First commit

---
+++ owner.gd
@@ -0,0 +1,15 @@
+extends CharacterBody2D
+
+
+const SPEED = 30.0
+
+func _physics_process(delta: float) -> void:
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var direction := Input.get_axis("Up", "Down")
+	if direction:
+		velocity.y = direction * SPEED
+	else:
+		velocity.y = move_toward(velocity.x, 0, SPEED)
+
+	move_and_collide(velocity)
